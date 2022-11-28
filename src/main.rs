3119f1b3a4e7027e20f48d05ea0916581c790e54
mcncm <33966432+mcncm@users.noreply.github.com>
2022-11-28 01:06:00 -0500

    Added gitignore, clean command, etc.

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ enum Command {
     New { name: String },
     Init { name: String },
     Build,
+    Clean,
 }
 
 #[derive(Deserialize)]
@@ -20,13 +21,16 @@ struct Config {
     project: Project,
 }
 
+// Must allow dead code because we want to deserialize e.g. `name`, not `_name`,
+// even if it isn't used.
+#[allow(dead_code)]
 #[derive(Deserialize)]
 struct Project {
-    _name: String,
+    name: String,
     build_command: String,
 }
 
-fn do_in_root<T, F: Fn(Config) -> T>(f: F) -> T {
+fn do_in_root<T, F: Fn(&Config) -> T>(f: F) -> T {
     let initial_path = std::env::current_dir().unwrap();
     let config_builder = config::Config::builder()
         .add_source(config::File::new("xargo.toml", config::FileFormat::Toml));
@@ -37,7 +41,7 @@ fn do_in_root<T, F: Fn(Config) -> T>(f: F) -> T {
                 let config = config
                     .try_deserialize()
                     .expect("failed to deserialize config");
-                return f(config);
+                return f(&config);
             }
             Err(e) => {
                 eprintln!("{}", e);
@@ -59,6 +63,8 @@ fn init_project(_name: &String) -> std::io::Result<()> {
     main.write_all(include_bytes!("files/main.tex"))?;
     let mut preamble = std::fs::File::create("src/preamble.tex")?;
     preamble.write_all(include_bytes!("files/preamble.tex"))?;
+    let mut gitignore = std::fs::File::create(".gitignore")?;
+    gitignore.write_all(include_bytes!("files/gitignore.txt"))?;
     // Prepare the build directory
     std::fs::create_dir("target")?;
     Ok(())
@@ -71,21 +77,32 @@ fn new_project(name: &String) -> std::io::Result<()> {
     init_project(&name)
 }
 
-fn build_project(conf: Config) -> std::io::Result<()> {
+fn build_command(conf: &Config) -> std::process::Command {
+    let mut cmd = std::process::Command::new(&conf.project.build_command);
+    let arg = String::from(r#""\input{main.tex}""#);
+    cmd.arg(&arg);
+    cmd
+}
+
+fn build_project(conf: &Config) -> std::io::Result<()> {
     // Copy the source directory to `target/src`
     std::process::Command::new("cp")
-        .args(["-r", "src/", "target/src/"])
+        .args(["-r", "src/", "target/build/"])
         .output()
         .expect("failed to copy src dir");
     // Build the project
     let project_dir = std::env::current_dir().unwrap();
-    std::env::set_current_dir("target/src").unwrap();
-    std::process::Command::new(&conf.project.build_command)
-        .arg("main.tex")
-        .output()
-        .expect("build command failed");
+    std::env::set_current_dir("target/build").unwrap();
+    build_command(&conf).output().expect("build command failed");
     std::env::set_current_dir(&project_dir).unwrap();
-    std::fs::copy("target/src/main.pdf", "target/main.pdf").expect("failed to copy target");
+    std::fs::copy("target/build/main.pdf", "target/main.pdf").expect("failed to copy target");
+    Ok(())
+}
+
+fn clean_project(_conf: &Config) -> std::io::Result<()> {
+    std::process::Command::new("rm")
+        .args(["-rf", "target/*"])
+        .output()?;
     Ok(())
 }
 
@@ -94,6 +111,7 @@ fn execute(cmd: &Command) -> std::io::Result<()> {
         Command::New { name } => new_project(&name),
         Command::Init { name } => init_project(&name),
         Command::Build => do_in_root(build_project),
+        Command::Clean => do_in_root(clean_project),
     }
 }
 
