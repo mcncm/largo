a06d64a1f436eb878bb136e3d198b10c5775e01a
mcncm <33966432+mcncm@users.noreply.github.com>
2023-03-05 12:47:33 -0500

    Factored out cli to separate module

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod build;
+pub mod cli;
 pub mod conf;
 pub mod dependencies;
 pub mod dirs;
@@ -6,3 +7,6 @@ pub mod files;
 pub mod project;
 pub mod util;
 pub mod vars;
+
+pub type Error = anyhow::Error;
+pub type Result<T> = anyhow::Result<T>;
